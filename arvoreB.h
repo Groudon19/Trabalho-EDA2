1817c8bfa23dc60db75f7683bf6fc9e17f1d1563
Lodetti <135389052+Lodetti@users.noreply.github.com>
2024-11-22 20:24:31 -0300

    biblioteca atualizada

--- arvoreB.h
+++ arvoreB.h
@@ -16,6 +16,7 @@ typedef struct arvoreB {
 ArvoreB* criaArvoreB(int ordem);
 NoB* criaNoB(ArvoreB* arvore);
 void percorreArvoreB(NoB* no, void (visita)(int chave));
+void imprime(int chave);
 int loclizaChave(ArvoreB* arvore, int chave);
 int pesquisaBinaria(NoB* no, int chave);
 NoB* localizaNoB(ArvoreB* arvore, int chave);
@@ -24,3 +25,14 @@ int transbordo(ArvoreB* arvore, NoB* no);
 NoB* divideNoB(ArvoreB* arvore, NoB* no);
 void adicionaChave(ArvoreB* arvore, int chave);
 void adicionaChaveRecursivo(ArvoreB* arvore, NoB* no, NoB* novo, int chave);
+void removeChave(ArvoreB* arvore, int chave);
+void removeChaveRecursivo(ArvoreB* arvore, NoB* no, int chave);
+int encontraPredecessor(NoB* no);
+int encontraSucessor(NoB* no);
+void fundirNos(ArvoreB* arvore, NoB* no, int indice);
+void redistribuirEsquerda(NoB* pai, NoB* filho, NoB* irmao, int indice);
+void redistribuirDireita(NoB* pai, NoB* filho, NoB* irmao, int indice);
+void imprimeDetalhesNo(NoB* no, int nivel);
+void imprimeDetalhesArvore(ArvoreB* arvore);
+int existe_no_vetor(int* vet, int tam, int valor);
+
